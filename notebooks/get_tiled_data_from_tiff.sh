cf8aad3b3b65e4137b03f5353edf21d084faf4a9
Michael Hany Elabd <mhelabd@stanford.edu>
2021-02-04 17:14:29 -0800

    computer cluster config

---
+++ notebooks/get_tiled_data_from_tiff.sh
@@ -0,0 +1,30 @@
+#!/bin/bash                                                                                                                                                                                                                                                                              #SBATCH --partition=tibet --qos=normal                                                                                                                                                                                                                                                   #SBATCH --time=06:00:00                                                                                                                                                                                                                                                                  #SBATCH --nodes=1                                                                                                                                                                                                                                                                        #SBATCH --cpus-per-task=2      
+#SBATCH --mem=8G
+
+# only use the following on partition with GPUs
+#SBATCH --gres=gpu:1
+
+#SBATCH --job-name="sample"
+#SBATCH --output=sample-%j.out
+
+# only use the following if you want email notification
+####SBATCH --mail-user=youremailaddress
+####SBATCH --mail-type=ALL
+
+# list out some useful information (optional)
+echo "SLURM_JOBID="$SLURM_JOBID
+echo "SLURM_JOB_NODELIST"=$SLURM_JOB_NODELIST
+echo "SLURM_NNODES"=$SLURM_NNODES
+echo "SLURMTMPDIR="$SLURMTMPDIR
+echo "working directory = "$SLURM_SUBMIT_DIR
+
+# sample process (list hostnames of the nodes you've requested)
+NPROCS=`srun --nodes=${SLURM_NNODES} bash -c 'hostname' |wc -l`
+echo NPROCS=$NPROCS
+
+# can try the following to list out which GPU you have access to
+python /sailhome/mhelabd/scaling-kilns/kiln-scaling/notebooks/get_tiled_data_from_tiff.py
+#srun /usr/local/cuda/samples/1_Utilities/deviceQuery/deviceQuery
+
+# done
+echo "Done"
