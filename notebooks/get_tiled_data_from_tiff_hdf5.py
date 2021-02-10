b735bef4bf7b1ea03d27e8bce549617b97fa6cf7
Michelle <michelleliu343@gmail.com>
2021-02-10 02:50:38 -0800

    verify hdf5 file transfer and update script

--- notebooks/get_tiled_data_from_tiff_hdf5.py
+++ notebooks/get_tiled_data_from_tiff_hdf5.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
+# In[203]:
+
+
+get_ipython().system('pip install geopy')
+
+
+# In[248]:
 
 
 import os
 import json
-import sys
-import requests
 import time
-from requests.auth import HTTPBasicAuth
 import pandas as pd
 import datetime
 import rasterio
@@ -17,12 +20,12 @@ from matplotlib import pyplot as plt
 import numpy as np
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
-from os import listdir, path
-from os.path import isfile, join
+from os import path
 import h5py
+import geopy.distance
 
 
-# In[8]:
+# In[2]:
 
 
 from pydrive.auth import GoogleAuth
@@ -46,18 +49,17 @@ gauth.SaveCredentialsFile("mycreds.txt")
 drive = GoogleDrive(gauth)
 
 
-# In[6]:
+# In[250]:
 
 
 # set params
 tile_height, tile_length = (64, 64)
-bands_to_read = None # ['B4', 'B3', 'B2'] # set to None to read all bands
 examples_per_save_file = 1000
 composite_file_name = 'bangladesh_all_bands_final'
 download_all_first = False
 
-save_path = '/atlas/u/{}/data/kiln-scaling/tiles/'.format(sys.argv[0])
-composite_save_path = '/atlas/u/{}/data/kiln-scaling/composites/'.format(sys.argv[0])
+save_path = '/atlas/u/mhelabd/data/kiln-scaling/tiles/'
+composite_save_path = '/atlas/u/mhelabd/data/kiln-scaling/composites/'
 
 # save_path = '../data/tiles_hdf5/'
 # composite_save_path = '../data/composites/'
@@ -65,12 +67,11 @@ composite_save_path = '/atlas/u/{}/data/kiln-scaling/composites/'.format(sys.arg
 # resources
 kilns = pd.read_csv("../data/bangladesh_kilns.csv")
 all_bands = ['B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8A', 'B8', 'B9', 'B10', 'B11', 'B12']
-band_dict = dict(zip(all_bands, range(1, len(all_bands) + 1)))
 
 print(kilns.head())
 
 
-# In[7]:
+# In[5]:
 
 
 file_list = drive.ListFile({'q': "title contains '" + composite_file_name + "'"}).GetList()
@@ -80,7 +81,7 @@ for file in file_list[:5]:
   print('title: %s, id: %s' % (file['title'], file['id']))
 
 
-# In[8]:
+# In[6]:
 
 
 # calculate image grid
@@ -92,7 +93,7 @@ print("Number of image grid columns:", num_image_cols)
 print("Number of image grid rows:", num_image_rows)
 
 
-# In[9]:
+# In[92]:
 
 
 coords = []
@@ -107,15 +108,13 @@ for sublist in coords:
     for coord in sublist:
         for c in coord:
             flat_coords.append(c)
-
+            
+flat_coords += [flat_coords[0]]
 bangladesh_geo = Polygon(flat_coords)
 
 
-# In[10]:
+# In[93]:
 
-
-# testing variables
-num_tiles_dropped = 0
 
 # optional pre-download all files
 if download_all_first:
@@ -133,10 +132,10 @@ if download_all_first:
     print("Done downloading all files.")
 
 
-# In[ ]:
+# In[228]:
 
 
-def get_bands_and_bounds_from_file(file, band_names=None):
+def get_bands_and_bounds_from_file(file):
     print("Starting file " + file['title'])
     composite_file_path = composite_save_path + file['title']
     if path.exists(composite_file_path):
@@ -150,15 +149,7 @@ def get_bands_and_bounds_from_file(file, band_names=None):
     # open file with rasterio
     print("Reading file...")
     dataset = rasterio.open(composite_file_path)
-    if band_names is None: # get all bands
-        bands = dataset.read()
-    else:
-        # TODO refactor using np.index_select
-        bands = []
-        for idx, band_name in enumerate(band_names):
-            band_index = band_dict[band_name]
-            bands += [dataset.read(band_index)]
-        bands = np.array(bands)
+    bands = dataset.read()
     print("Done processing file")
     return bands, dataset.bounds
 
@@ -171,14 +162,21 @@ def get_data_and_bounds_given_pixels(ds_bounds, bands, start_row, start_col, con
     tile_left = ds_bounds.left + (start_col / ds_length) * (ds_bounds.right - ds_bounds.left)
     tile_right = ds_bounds.left + ((start_col + tile_length) / ds_length) * (ds_bounds.right - ds_bounds.left)
     bounds = np.array([tile_bottom, tile_left, tile_top, tile_right])
-    tile_coordinates = [[tile_left, tile_top], [tile_right, tile_top], [tile_right, tile_bottom], [tile_left, tile_bottom]]
-#     print("top", tile_top, "bottom", tile_bottom, "left", tile_left, "right", tile_right)
-    if not contains_kiln:
-        for point in [Point([c[0], c[1]]) for c in tile_coordinates]:
-            if not bangladesh_geo.contains(point):
-                num_tiles_dropped += 1
-                return None, None
+    tile_geo = Polygon([[tile_left, tile_top], [tile_right, tile_top], [tile_right, tile_bottom], [tile_left, tile_bottom], [tile_left, tile_top]])
+    if not contains_kiln and not bangladesh_geo.intersects(tile_geo):
+        num_tiles_dropped += 1
+        return None, None
     return bands[:, start_row : start_row + tile_height, start_col : start_col + tile_length], bounds
+
+def save_current_file(save_index, counter):
+    filename = save_path + "examples_" + str(save_index) + ".hdf5"
+    print("Saving file", filename)
+    f = h5py.File(filename, 'w')
+    bounds_dset = f.create_dataset("bounds", data=tile_bounds[:counter])
+    examples_dset = f.create_dataset("images", data=examples[:counter])
+    labels_dset = f.create_dataset("labels", data=labels[:counter])
+    f.close()
+    return save_index + 1, 0
 
 def add_example(ex_data, ex_bounds, save_index, counter, is_positive):
     tile_bounds[counter] = ex_bounds
@@ -186,127 +184,160 @@ def add_example(ex_data, ex_bounds, save_index, counter, is_positive):
     labels[counter] = 1 if is_positive else 0
     new_counter = counter + 1
     
-    # save files if needed
     if new_counter == examples_per_save_file:
-        filename = save_path + "examples_" + str(save_index) + ".hdf5"
-        print("Saving file", filename)
-        f = h5py.File(filename, 'w')
-        bounds_dset = f.create_dataset("bounds", data=tile_bounds)
-        examples_dset = f.create_dataset("images", data=examples)
-        labels_dset = f.create_dataset("labels", data=labels)
-        f.close()
-        
-        save_index += 1
-        new_counter = 0
-    return new_counter, save_index
+        return save_current_file(save_index, counter)
+    return save_index, new_counter
 
-def get_kiln_tiles(bounds, num_rows, num_cols):
+def get_kiln_tiles_approx_coords(bounds, num_rows, num_cols):
     kilns_in_image = kilns.loc[(kilns['lat'] >= bounds['bottom']) & (kilns['lat'] <= bounds['top']) 
         & (kilns['lon'] >= bounds['left']) & (kilns['lon'] <= bounds['right'])]
     
-    tiles = set() # set of tuples of (tile_row, tile_col)
+    tiles = set()
     for index, kiln in kilns_in_image.iterrows():
-        lon_percent = 1 - (kiln['lon'] - bounds['left']) / (bounds['right'] - bounds['left'])
-        row_index = int(num_rows * lon_percent)
+        kiln_pos = (kiln['lat'], kiln['lon'])
+        kiln_to_top = geopy.distance.geodesic(kiln_pos, (bounds['top'], kiln_pos[1])).km
+        kiln_to_bottom = geopy.distance.geodesic(kiln_pos, (bounds['bottom'], kiln_pos[1])).km
+        row_index = int(kiln_to_top / (kiln_to_top + kiln_to_bottom) * num_rows)
         
-        lat_percent = 1 - (kiln['lat'] - bounds['bottom']) / (bounds['top'] - bounds['bottom'])
-        col_index = int(num_cols * lat_percent)
+        kiln_to_left = geopy.distance.geodesic(kiln_pos, (kiln_pos[0], bounds['left'])).km
+        kiln_to_right = geopy.distance.geodesic(kiln_pos, (kiln_pos[0], bounds['right'])).km
+        col_index = int(kiln_to_left / (kiln_to_left + kiln_to_right) * num_cols)
         tiles.add((row_index, col_index))
     return tiles
 
 
-# In[ ]:
+# In[126]:
 
+
+## testing & visualization methods
+
+# image is a single example of shape (13, 64, 64)
+def visualize_tile(image, indices=[3, 2, 1]):
+    row_idx = np.array(indices)
+    X = np.transpose(image[row_idx], (1, 2, 0))
+    X *= 1 / np.max(X)
+    print(X.shape)
+    plt.imshow(X)
+    
+def pretty_bounds(bounds):
+    return [[bounds[1], bounds[0]], [bounds[1], bounds[2]], [bounds[3], bounds[2]], [bounds[3], bounds[0]], [bounds[1], bounds[0]]]
+
+
+# In[309]:
+
+
+## testing variables
+num_tiles_dropped = 0
+pos_examples = []
 
 save_index, counter = 0, 0
 
 tile_bounds = np.zeros([examples_per_save_file, 4])
-examples = np.zeros([examples_per_save_file, len(all_bands) if bands_to_read is None else len(bands_to_read), tile_height, tile_length])
+examples = np.zeros([examples_per_save_file, len(all_bands), tile_height, tile_length])
 labels = np.zeros([examples_per_save_file, 1])
 
 
-# In[ ]:
+# In[233]:
 
 
-# file_list = file_list[:1] # testing purposes
+file_list = file_list[:1] # testing purposes
 
 total_start_time = time.time()
 for index, file in enumerate(file_list):
     file_start_time = time.time()
-    bands, ds_bounds = get_bands_and_bounds_from_file(file, band_names=bands_to_read)
+    bands, ds_bounds = get_bands_and_bounds_from_file(file)
     
-    file_in_country = False
-    file_coordinates = [[ds_bounds.left, ds_bounds.top], [ds_bounds.right, ds_bounds.top], [ds_bounds.right, ds_bounds.bottom], [ds_bounds.left, ds_bounds.bottom]]
-    for point in [Point([c[0], c[1]]) for c in file_coordinates]:
-        if bangladesh_geo.contains(point):
-            file_in_country = True
-    
-    if file_in_country:
-        num_bands, ds_height, ds_length = bands.shape
-        num_rows = ds_height // tile_height
-        num_cols = ds_length // tile_length
+    num_bands, ds_height, ds_length = bands.shape
+    num_rows = ds_height // tile_height
+    num_cols = ds_length // tile_length
 
-        bounds = {
-            "bottom": ds_bounds.bottom,
-            "top": ds_bounds.top,
-            "left": ds_bounds.left,
-            "right": ds_bounds.right
-        }
+    bounds = {
+        "bottom": ds_bounds.bottom,
+        "top": ds_bounds.top,
+        "left": ds_bounds.left,
+        "right": ds_bounds.right
+    }
 
-        row_px_excess, col_px_excess = None, None
-        percent_row_excess, percent_col_excess = None, None
+    row_px_excess, col_px_excess = None, None
+    percent_row_excess, percent_col_excess = None, None
 
-        if index % num_image_cols == num_image_cols - 1: # rightmost column
-            # calculate excess col pixels
-            col_px_excess = ds_length % tile_length
-            bounds["right"] -= col_px_excess / ds_length * (ds_bounds.right - ds_bounds.left)
+    if index % num_image_cols == num_image_cols - 1: # rightmost column
+        # calculate excess col pixels
+        col_px_excess = ds_length % tile_length
+        bounds["right"] -= col_px_excess / ds_length * (ds_bounds.right - ds_bounds.left)
 
-        if index // num_image_cols == num_image_rows - 1: # last row
-            # calculate excess row pixels
-            row_px_excess = ds_height % tile_height
-            bounds["bottom"] += row_px_excess / ds_height * (ds_bounds.top - ds_bounds.bottom)
+    if index // num_image_cols == num_image_rows - 1: # last row
+        # calculate excess row pixels
+        row_px_excess = ds_height % tile_height
+        bounds["bottom"] += row_px_excess / ds_height * (ds_bounds.top - ds_bounds.bottom)
 
-        kiln_tiles = get_kiln_tiles(bounds, num_rows, num_cols)
-        print("Num tiles with kilns:", len(kiln_tiles))
-        print(kiln_tiles)
+    kiln_tiles = get_kiln_tiles_approx_coords(bounds, num_rows, num_cols)
+    print("Num tiles with kilns:", len(kiln_tiles))
+    print(kiln_tiles)
 
-        print("Tiling dataset...")
-        for tile_idx_row in range(0, num_rows):
-            px_row = tile_idx_row * tile_height
-            for tile_idx_col in range(0, num_cols):
-                px_col = tile_idx_col * tile_length
-                tile_has_kiln = (tile_idx_row, tile_idx_col) in kiln_tiles
-                data, data_bounds = get_data_and_bounds_given_pixels(ds_bounds, bands, px_row, px_col, tile_has_kiln)
-                if data is not None:
-                    counter, save_index = add_example(data, data_bounds, save_index, counter, tile_has_kiln)
-                
-        # TODO: handle leftovers
+    print("Tiling dataset...")
+    for tile_idx_row in range(0, num_rows):
+        px_row = tile_idx_row * tile_height
+        for tile_idx_col in range(0, num_cols):
+            px_col = tile_idx_col * tile_length
+            tile_has_kiln = (tile_idx_row, tile_idx_col) in kiln_tiles
+            data, data_bounds = get_data_and_bounds_given_pixels(ds_bounds, bands, px_row, px_col, tile_has_kiln)
+            if data is not None:
+                save_index, counter = add_example(data, data_bounds, save_index, counter, tile_has_kiln)
+                if tile_has_kiln:
+                    print("index", (tile_idx_row, tile_idx_col))
+                    print("bounds", pretty_bounds(data_bounds))
+                    pos_examples += [data]
+                    pos_example_indices = 
 
-        print("Total tiles dropped (outside country):", num_tiles_dropped)
-        print("Total tiles kept:", str(num_rows * num_cols - num_tiles_dropped))
-        num_tiles_dropped = 0
-        print("Finished file in", time.time() - file_start_time, "\n")
+    # handle leftovers in a final file
+    if index == len(file_list) - 1:
+        save_current_file(save_index, counter)
+
+    print("Total tiles dropped (outside country):", num_tiles_dropped)
+    print("Total tiles kept:", str(num_rows * num_cols - num_tiles_dropped))
+    num_tiles_dropped = 0
+    print("Finished file in", time.time() - file_start_time, "\n")
 print("Finished " + str(len(file_list)) + " files in: " + str(time.time() - total_start_time))
 
 
-# In[ ]:
+# ## Test hdf5 file data format & visualizations
+
+# In[244]:
 
 
-# f = h5py.File('../data/tiles_hdf5/examples_0.hdf5','r')
-# print("all keys", f.keys())
-# print(f['images'].shape)
-# print(f['bounds'].shape)
-# print(f['labels'].shape)
+visualize_tile(pos_examples[10])
+# visualize_tile(examples[1])
+# visualize_tile(examples[2])
 
 
-# In[ ]:
+# In[304]:
 
 
-print(tile_bounds.shape)
+WANTED_BANDS = [3, 2, 1]
+
+for i in range(5):
+    with h5py.File(save_path + "examples_" + str(i) + ".hdf5", "r") as f:
+        if i == 0:
+            X = np.array(f["images"][()])                .reshape((-1, len(all_bands), 64, 64))
+            X = np.moveaxis(X, 1, -1)[:, :, :, WANTED_BANDS]
+            y = np.array(f["labels"][()])
+        else:
+            x_i = np.array(f["images"][()])                .reshape((-1, len(all_bands), 64, 64))
+            x_i = np.moveaxis(x_i, 1, -1)[:, :, :, WANTED_BANDS]
+            X = np.concatenate((X, x_i))
+            y_i = np.array(f["labels"][()])
+            y = np.concatenate((y, y_i))
 
 
-# In[ ]:
+# In[315]:
 
 
-
+print("Examples w/ kilns:", np.where(y==1.0)[0])
+index = 4321
+print("label:", y[index])
+vis_X = X[index]
+vis_X *= 1 / np.max(vis_X)
+print(vis_X.shape)
+plt.imshow(vis_X)
 
